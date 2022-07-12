1059329589076c8533afebd6325d92585d0b9eae
vattipallysushmitha <vengalreddysushmitha@gmail.com>
2022-07-12 12:18:45 +0530

    results stored in dictionary to array

--- arthamaticsortings.sh
+++ arthamaticsortings.sh
@@ -13,10 +13,21 @@ echo "a*b+c=" $abc
 rst=$(awk 'BEGIN {print '$C' + '$A' / '$B'}')
 echo "c+a/b=" $rst
 mno=$(awk 'BEGIN {print '$A' % '$B' + '$C'}')
-echo "a%b+c=" $rst
+echo "a%b+c=" $mno
 declare -A arthamatic
 arthamatic[add]=$xyz
 arthamatic[multiply]=$abc
 arthamatic[divide]=$rst
 arthamatic[module]=$mno
 echo "results in dictionary are=${arthamatic[@]}"
+declare -A value
+declare -a values
+value[add]=$xyz
+values[0]=$value
+value[multiply]=$abc
+values[1]=$value
+value[divide]=$rst
+values[2]=$value
+value[module]=$mno
+values[3]=$value
+echo "values stored in dictionary to array ${value[add]} ${value[multiply]} ${value[divide]} ${value[module]}"
